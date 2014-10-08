5008ce78aaf2c7d3c620896ab4f7d636a5f1382f
Igor Zinin <igor.zinin@gmail.com>
2014-10-08 10:24:47 +0300

    progressive search implementation has been started

--- src/js/app.js
+++ src/js/app.js
@@ -1,21 +1,42 @@
 (function() {
-  var app = angular.module('cosmethics', []);
+  var underscore = angular.module('underscore', []);
+  underscore.factory('_', function() {
+    return window._; // assumes underscore has already been loaded on the page
+  });
+  var app = angular.module('cosmethics', ['underscore']);
+  
+  app.factory('singleton', function($rootScope) {
+	  var singleton = {
+		  searchDBVal: "",
+		  serachDBPrev: ""
+	  };
+	  return singleton;
+  });
 
-  app.directive("storeList", ['$http', '$log', function($http, $log){
-	return {
-		restrict: 'E',
-		templateUrl: "/templates/store-list-products.html",
-		controller:   function(){
-		    var self = this;
-		    $http.get("/readdb").success(function(data){
-		    	self.products = data.store;
-		    	$log.log(self.products);
-		    }).error(function(){
-		    	alert("error");
-		    });    
+  app.directive("storeList",['singleton', '$http', '$log', 
+                                       function(singleton, $http, $log){
+	  return {
+		  restrict: "E",
+		  templateUrl: "/templates/store-list-products.html",
+		  controller: function(){
+			  //guard code
+			  if(!_.isEmpty(singleton.searchDBVal) && singleton.searchDBVal == singleton.serachDBPrev){
+			    return;
+			  }
+			  
+			  var self = this;
+			  var par = (singleton.searchDBVal == singleton.serachDBPrev) ? 
+					  {filter:"ALL"} : {filter:"singleton.searchDBVal"};
+			  $http.get("/readdb", {params: par})
+			  .success(function(data){
+				  self.products = data.store;
+				  $log.log(self.products);
+			  }).error(function(){
+				  alert("server error");
+			  });
 		  },
-		controllerAs:"store"
-	};
-  }])
+		  controllerAs:"store"
+	  };
+  }]);
 	
 })();
